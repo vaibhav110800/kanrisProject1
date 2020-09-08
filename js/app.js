3737ea7925aa41f69d8a30765544f3c95cb28d28
vaibhav110800 <vibhu1108@gmail.com>
2020-09-08 16:13:19 +0530

    commit 4
    completed website

--- js/app.js
+++ js/app.js
@@ -24,3 +24,51 @@ let x = setInterval(function () {
     document.getElementById("minute").innerText = minutes;
     document.getElementById("second").innerText = seconds;
 }, 1000);
+
+
+//--------------------------- memory
+
+// Open the Modal
+function openModal() {
+    document.getElementById("mymem-Modal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById("mymem-Modal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demons");
+    var captionText = document.getElementById("mem-caption");
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
+}
